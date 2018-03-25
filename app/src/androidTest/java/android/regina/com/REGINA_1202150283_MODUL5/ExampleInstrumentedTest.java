c302737c5dfb37e7c7ef5454560747527d1471c6
reginayf <regina.yusticia40@gmail.com>
2018-03-25 21:21:55 +0700

    push pertama kali

---
+++ app/src/androidTest/java/android/regina/com/REGINA_1202150283_MODUL5/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package android.regina.com.REGINA_1202150283_MODUL5;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("android.lalita.com.idewaayulalitamarmika_1202154355_studycase5", appContext.getPackageName());
+    }
+}
